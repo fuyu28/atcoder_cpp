e7e6158a431e61cd487e45dbdd77bbc4da7c480e
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-05-02 00:33:41 +0900

    practice

--- APG4b/ex17/main.cpp
+++ APG4b/ex17/main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
+    int N, S;
+    cin >> N >> S;
+    vector<int> A(N), P(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A.at(i);
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> P.at(i);
+    }
 
-    return 0;
+    // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
+    // ここにプログラムを追記
 }
