6c00b69986156ff2c6bbf18639b04c6b5220007d
yuuzora <88916226+fuyu28@users.noreply.github.com>
2024-05-02 00:33:41 +0900

    practice

--- APG4b/ex16/main.cpp
+++ APG4b/ex16/main.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
+    vector<int> data(5);
+    for (int i = 0; i < 5; i++) {
+        cin >> data.at(i);
+    }
 
-    return 0;
+    // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+    // ここにプログラムを追記
+    int equal = 0;
+    for  (int i=0;i<data.size()-1;i++) {
+        if (data.at(i) == data.at(i+1)) {
+            cout << "YES" << endl;
+            equal = 1;
+            break;        }
+    }
+    if (equal == 0) cout << "NO" << endl;
 }
