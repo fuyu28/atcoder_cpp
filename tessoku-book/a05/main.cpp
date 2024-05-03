230933173ac390471bd33bd92e7040c53ec9d8a3
yuuzora <88916226+fuyu28@users.noreply.github.com>
2024-05-03 17:22:26 +0900

    chapter1 end

--- tessoku-book/a05/main.cpp
+++ tessoku-book/a05/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-
+    int N,K;
+    cin >> N >> K;
+    int answer = 0;
+    for (int i=1;i<=N;i++) {
+        for (int j=1;j<=N;j++) {
+            int k = K - i - j;
+            if (k >= 1 && k <= N) answer++;
+        }
+    }
+    cout << answer << endl;
     return 0;
 }
