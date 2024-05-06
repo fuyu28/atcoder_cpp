dc262cc1325a65455380e047649203a843c37fae
nayuta <88916226+fuyu28@users.noreply.github.com>
2024-05-06 20:10:25 +0900

    add abc351 code

--- abc351/a/main.cpp
+++ abc351/a/main.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
-
-    return 0;
+   int A[20],B[20];
+   int Asum,Bsum = 0;
+   
+   for (int i=0;i<9;i++) {
+     cin >> A[i];
+     Asum += A[i];
+   }
+   
+   for (int i=0;i<8;i++) {
+     cin >> B[i];
+     Bsum += B[i];
+   }
+   
+   cout << Asum - Bsum +1 << endl;
+   return 0;
 }
--- abc351/b/main.cpp
+++ abc351/b/main.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 
 int main() {
-
-    return 0;
+  int N;
+  cin >> N;
+  
+  char A[N][N],B[N][N];
+  
+  for (int i=0;i<N;i++) {
+    for (int j=0;j<N;j++) {
+      cin >> A[i][j];
+    }
+  }
+  
+  for (int i=0;i<N;i++) {
+    for (int j=0;j<N;j++) {
+      cin >> B[i][j];
+    }
+  }
+  
+  for (int i=0;i<N;i++) {
+    for (int j=0;j<N;j++) {
+      if (A[i][j] != B[i][j]) {
+        cout << i+1 << " " << j+1 <<endl;
+        break;
+      }
+    }
+  }
+  
+  return 0;
 }
