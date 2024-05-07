a3339f0d084e8b6b34993e482474c4e1925228e6
yuuzora <88916226+fuyu28@users.noreply.github.com>
2024-05-07 18:38:06 +0900

    write abc083

--- abc083/b/main.cpp
+++ abc083/b/main.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Getdigit(int num) {
-    int digit = 0;
-    while (num != 0) {
-        num /= 10;
-        digit++;
+int findSumOfDigits (int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
     }
-    return digit;
+    return sum;
 }
 
 int main() {
     int N, A, B;
     cin >> N >> A >> B;
-    int answer = 0;
-    for (int i = 0; i < N; i++) {
-        
-        for (int j = 0; j < Getdigit(i); j++) {
-
+    int total = 0;
+    for (int i = 1; i <= N; ++i) {
+        int sum = findSumOfDigits(i);
+        if (A <= sum && sum <= B) {
+            total += i;
         }
     }
+    cout << total << endl;
     return 0;
 }
