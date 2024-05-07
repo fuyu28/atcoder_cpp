00a1a0c828bb95b70c6242b0b4938d2cb06e752b
yuuzora <88916226+fuyu28@users.noreply.github.com>
2024-05-08 00:03:53 +0900

    solve abc088

---
+++ abc088/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N,A = 0;
+  cin >> N >> A ;
+  if (N % 500 <= A) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
