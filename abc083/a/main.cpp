485161c87fa85492f4e97a8d34e4b91a09c9881f
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-05-07 18:38:06 +0900

    write abc083

--- abc083/a/main.cpp
+++ abc083/a/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    
-    return 0;
+  int A,B,C,D = 0;
+  cin >> A >> B >> C >> D;
+  if (A + B == C + D) {
+    cout << "Balanced" << endl;
+  }
+  else if (A + B > C + D) {
+    cout << "Left" << endl;
+  }
+  else if (A + B < C + D) {
+    cout << "Right" << endl;
+  }
 }
