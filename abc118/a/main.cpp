0fea4df0e34d5992af4ce44cf69d6cc87bf3ed15
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-05-11 20:15:49 +0900

    solve abc118 ab

--- abc118/a/main.cpp
+++ abc118/a/main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
+    int A, B;
+    cin >> A >> B;
 
+    if (B % A == 0 ) cout << A + B << endl;
+    else cout << B - A << endl;
     return 0;
 }
--- abc118/b/main.cpp
+++ abc118/b/main.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main() {
+    int N, M, K, A;
+    cin >> N >> M;
+    vector<int> exist(M + 1, 0);
+    int cnt = 0;
+
+    for (int i = 0; i < N; ++i) {
+        cin >> K;
+        for (int j = 1; j <= K; ++j) {
+            cin >> A;
+            exist.at(A)++;
+        }
+    }
+
+    for (int i = 1; i <= M; ++i) {
+        if (exist.at(i) == N) cnt++;
+    }
+
+    cout << cnt << endl;
 
     return 0;
 }
