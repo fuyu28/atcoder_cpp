b2f9bb3d00fae46504e810cf0894ae094b84fc63
yuuzora <88916226+fuyu28@users.noreply.github.com>
2024-05-19 00:27:56 +0900

    arc177

---
+++ arc177/a/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int A, B, C, D, E, F, N;
+    cin >> A >> B >> C >> D >> E >> F >> N;
+    vector<int> X(N);
+
+    ll sum = 1 * A + 5 * B + 10 * C + 50 * D + 100 * E + 500 * F;
+
+    for (auto& v : X) cin >> v;
+
+    for (int i = 0; i < N; ++i) {
+        
+    }
+
+    return 0;
+}
