0ca6cf35a23982da31cba37e2546c5728baa46a6
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-06-25 10:40:51 +0900

    rewrite abc358 d

--- abc358/d/main.cpp
+++ abc358/d/main.cpp
@@ -16,26 +16,21 @@ int main() {
     for(auto& x : B) cin >> x;
     sort(all(A)); sort(all(B));
 
-    vector<ll> A_(N);
+    vector<ll> usedA(N);
     ll cnt = 0; ll cnt_a = 0; ll ans = -1;
     rep(cnt_b, M) {
         while(cnt_a < N) {
             if(A.at(cnt_a) >= B.at(cnt_b)) {
-                A_.at(cnt) = A.at(cnt_a);
+                usedA.at(cnt) = A.at(cnt_a);
                 ++cnt; ++cnt_a;
                 break;
             }
             ++cnt_a;
         }
     }
-    int cnt_a_ = 0;
-    for(auto x : A_) {
-        if(x != 0) ++cnt_a_;
-    }
-    if(cnt_a_ == M) ans = 0;
-    if(ans == 0) {
-        for(auto i : A_) ans += i;
-    }
+    int sz_usedA = count_if(all(usedA),[](ll x) {return x != 0;});
+    if(sz_usedA == M) ans = 0;
+    if(ans == 0) ans = accumulate(all(usedA),0);
     cout << ans << endl;
     return 0;
 }
