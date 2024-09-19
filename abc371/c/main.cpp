924ff9bd6d4b83278b4bb87dd0bae483f060f19c
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-09-19 13:22:19 +0900

    add contests

---
+++ abc371/c/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    
+    return 0;
+}
