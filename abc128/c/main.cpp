aa8df296ef005a9eb14b50c570467ef5693793ce
nayuta <88916226+fuyu28@users.noreply.github.com>
2024-10-15 17:58:07 +0900

    solve abc128,abc147

--- abc128/c/main.cpp
+++ abc128/c/main.cpp
@@ -9,6 +9,44 @@ typedef long long ll;
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> s(m);
+    vector<int> p(m);
+    rep(i, m) {
+        int k;
+        cin >> k;
+        vector<int> ss(k);
+        rep(j, k) {
+            cin >> ss[j];
+            --ss[j];
+        }
+        s[i] = ss;
+    }
+    rep(i, m) {
+        cin >> p[i];
+    }
+
+    int ans = 0;
+    for (int bit = 0; bit < (1 << n); ++bit) {
+        bitset<10> sw(bit);
+        bool ok = true;
+        rep(i, m) {
+            int cnt = 0;
+            rep(j, sz(s[i])) {
+                if (sw.test(s[i][j])) {
+                    ++cnt;
+                }
+            }
+            if (cnt % 2 != p[i]) {
+                ok = false;
+            }
+        }
+        if (ok) {
+            ++ans;
+        }
+    }
+
+    cout << ans << endl;
     return 0;
 }
--- abc147/c/main.cpp
+++ abc147/c/main.cpp
@@ -9,6 +9,43 @@ typedef long long ll;
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-    
+    int n;
+    cin >> n;
+    vector<vector<pair<int, int>>> people(n);
+    rep(i, n) {
+        int a;
+        cin >> a;
+        rep(j, a) {
+            int x, y;
+            cin >> x >> y;
+            --x;
+            people[i].push_back({x, y});
+        }
+    }
+
+    int ans = 0;
+    for(int bit = 0; bit < (1 << n); ++bit) {
+        bitset<15> honest(bit);
+        bool ok = true;
+        for(int i = 0; i < n; ++i) {
+            if(!honest.test(i)) continue;
+            for(auto p : people[i]) {
+                int x = p.first;
+                int y = p.second;
+                if(honest.test(x) && y == 0) {
+                    ok = false;
+                }
+                if(!honest.test(x) && y == 1) {
+                    ok = false;
+                }
+            }
+        }
+        if(ok) {
+            cout << honest.count() << endl;
+            ans = max(ans, (int)honest.count());
+        }
+    }
+
+    cout << ans << endl;
     return 0;
 }
