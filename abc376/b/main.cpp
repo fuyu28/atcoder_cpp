c4a838199ad80acfd490086a27f0a644fab8d116
Yu Yamaguchi <88916226+fuyu28@users.noreply.github.com>
2024-11-05 14:49:54 +0900

    solve abc376

--- abc376/b/main.cpp
+++ abc376/b/main.cpp
@@ -6,75 +6,32 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 typedef long long ll;
 
+int move(int n, int from, int to, int ng) {
+        if (from > to) swap(from, to);
+        if (from < ng && ng < to) {
+            return n + from - to;
+        } else {
+            return to - from;
+        }
+    }
+
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     
     int n, q;
     cin >> n >> q;
-    vector<string> h(q);
-    vector<int> t(q);
-    rep(i, q) {
-        cin >> h.at(i) >> t.at(i);
-    }
-    int L = 1, R = 2;
     int ans = 0;
+    int l = 1, r = 2;
+    string h; int t;
 
     rep(i, q) {
-        int cnt = 0;
-        if(h.at(i) == "L") {
-            int tmp = L;
-            while(true) {
-                tmp++; cnt++;
-                if(tmp > n) tmp -= n;
-                if(tmp == R) {
-                    cnt = 0;
-                    break;
-                }
-                if(tmp == t.at(i)) {
-                    L = tmp;
-                    ans += cnt;
-                    break;
-                }
-            }
-            if(cnt == 0) {
-                int tmp = L;
-                while(true) {
-                    tmp--; cnt++;
-                    if(tmp < 1) tmp += n;
-                    if(tmp == t.at(i)) {
-                        L = tmp;
-                        ans += cnt;
-                        break;
-                    }
-                }
-            }
+        cin >> h >> t;
+        if (h == "L") {
+            ans += move(n, l, t, r);
+            l = t;
         } else {
-            int tmp = R;
-            while(true) {
-                tmp--; cnt++;
-                if(tmp < 1) tmp += n;
-                if(tmp == L) {
-                    cnt = 0;
-                    break;
-                }
-                if(tmp == t.at(i)) {
-                    R = tmp;
-                    ans += cnt;
-                    break;
-                }
-            }
-            if(cnt == 0) {
-                int tmp = R;
-                while(true) {
-                    tmp++; cnt++;
-                    if(tmp > n) tmp -= n;
-                    if(tmp == t.at(i)) {
-                        R = tmp;
-                        ans += cnt;
-                        break;
-                    }
-                }
-            }
+            ans += move(n, r, t, l);
+            r = t;
         }
     }
     cout << ans << endl;
