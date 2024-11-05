5ca31ea539828229f760b1d67f281523c10a492c
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-11-05 14:49:54 +0900

    solve abc376

--- abc376/c/main.cpp
+++ abc376/c/main.cpp
@@ -9,5 +9,28 @@ typedef long long ll;
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n-1);
+    rep(i, n) cin >> a.at(i);
+    rep(i, n-1) cin >> b.at(i);
+    sort(all(a)), sort(all(b));
+
+    int ans = -1;
+    rep(i, n-1) {
+        if (a.at(i) > b.at(i)) {
+            cout << ans << endl;
+            return 0;
+        }
+    }
+    for(int i = n-1; i > 0; --i) {
+        if (a.at(i) > b.at(i-1)) {
+            ans = a.at(i);
+            cout << ans << endl;
+            return 0;
+        }
+    }
+    ans = a.at(0);
+    cout << ans << endl;
     return 0;
 }
