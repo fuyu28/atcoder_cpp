dcb892e295464ef60090b3eeb4d76e8c8f2f98f0
fuyu28 <88916226+fuyu28@users.noreply.github.com>
2024-11-12 16:57:01 +0900

    update abc379

--- abc379/b/main.cpp
+++ abc379/b/main.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-typedef long long ll;
+#define sz(x) (lint)(x).size()
+#define rep(i, x, limit) for (lint i = (lint)x; i < (lint)limit; i++)
+#define REP(i, x, limit) for (lint i = (lint)x; i <= (lint)limit; i++)
+using lint = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<lint, lint>;
+const int dy[4] = {-1,0,1,0};
+const int dx[4] = {0,1,0,-1};
 
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
