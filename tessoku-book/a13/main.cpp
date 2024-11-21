39e8d13abbafcde98b47488b170013a297de8767
fuyu <88916226+fuyu28@users.noreply.github.com>
2024-11-22 02:03:43 +0900

    update abc379

--- tessoku-book/a13/main.cpp
+++ tessoku-book/a13/main.cpp
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(x) x.begin(), x.end()
+#define all(x) begin(x), end(x)
 #define sz(x) (lint)(x).size()
-#define rep(i, x, limit) for (lint i = (lint)x; i < (lint)limit; i++)
-#define REP(i, x, limit) for (lint i = (lint)x; i <= (lint)limit; i++)
+#define rep(i, x, y) for (lint i = (lint)x; i < (lint)y; i++)
+#define REP(i, x, y) for (lint i = (lint)x; i <= (lint)y; i++)
 using lint = long long;
-using ull = unsigned long long;
+using ulint = unsigned long long;
 using pii = pair<int, int>;
 using pll = pair<lint, lint>;
 const int dy[4] = {-1,0,1,0};
 const int dx[4] = {0,1,0,-1};
+struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); } }init;
 
 int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
     lint n, k;
-    vector<lint> a(100009), r(100009);
     cin >> n >> k;
-    REP(i, 1, n) cin >> a[i];
+    vector<lint> a(n), r(n);
+    rep(i, 0, n) cin >> a[i];
+    sort(all(a));
 
-    rep(i, 1, n) {
-        if (i == 1) r[i] = 1;
+    rep(i, 0, n-1) {
+        if (i == 0) r[i] = 0;
         else r[i] = r[i-1];
         while (r[i] < n && a[r[i]+1] - a[i] <= k) {
             r[i]++;
         }
     }
     lint ans = 0;
-    rep(i, 1, n) {
-        ans += (r[i] - i);
-    }
+    rep(i, 0, n-1) ans += (r[i] - i);
     cout << ans << endl;
     return 0;
 }
